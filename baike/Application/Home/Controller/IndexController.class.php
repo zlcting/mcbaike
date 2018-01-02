c6605606e582acf2958c75163fe48aeda471f436
zlcting <zlcting@gmail.com>
2018-01-02 16:51:23 +0800

    123123

--- baike/Application/Home/Controller/IndexController.class.php
+++ baike/Application/Home/Controller/IndexController.class.php
@@ -24,13 +24,28 @@ class IndexController extends Controller {
 
 
     public function res(){
-        //p(APP_PATH);exit;
-        //import("Lib.Ucclient.client");
-        //include APP_PATH."Lib/Ucclient/client.class.php";
         $uc_api = new \Lib\Ucclient\client();
-        $uid = $uc_api->uc_user_register(123123, 123123, 321231);
+        $uid = $uc_api->uc_user_register('test_user', '123', 'test@163.com');
+        if($uid <= 0) {
+            if($uid == -1) {
+                echo '用户名不合法';
+            } elseif($uid == -2) {
+                echo '包含要允许注册的词语';
+            } elseif($uid == -3) {
+                echo '用户名已经存在';
+            } elseif($uid == -4) {
+                echo 'Email 格式有误';
+            } elseif($uid == -5) {
+                echo 'Email 不允许注册';
+            } elseif($uid == -6) {
+                echo '该 Email 已经被注册';
+            } else {
+                echo '未定义';
+            }
+        } else {
+            echo '注册成功';
+        }
         p($uid);exit;
-        
     }
 
 }
