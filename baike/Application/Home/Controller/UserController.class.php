c8bb3982788e4532736a8e7d61f33b5018b80790
zlcting <zlcting@gmail.com>
2018-01-03 10:25:58 +0800

    user

---
+++ baike/Application/Home/Controller/UserController.class.php
@@ -0,0 +1,48 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    public function index(){
+        $name = 'ThinkPHP';
+        $this->assign('name',$name);
+        $this->display();
+    }
+
+    public function login(){
+
+
+        $this->display();
+
+    }
+
+    public function register(){
+
+        $this->display();
+    }
+
+    public function updateRegister(){
+        $uc_api = new \Lib\Ucclient\client();
+        $uid = $uc_api->uc_user_register('test_user', '123', 'test@163.com');
+        if($uid <= 0) {
+            if($uid == -1) {
+                echo '用户名不合法';
+            } elseif($uid == -2) {
+                echo '包含要允许注册的词语';
+            } elseif($uid == -3) {
+                echo '用户名已经存在';
+            } elseif($uid == -4) {
+                echo 'Email 格式有误';
+            } elseif($uid == -5) {
+                echo 'Email 不允许注册';
+            } elseif($uid == -6) {
+                echo '该 Email 已经被注册';
+            } else {
+                echo '未定义';
+            }
+        } else {
+            echo '注册成功';
+        }
+        p($uid);exit;
+    }
+
+}
