fae5c9d6d905f501bc97e5be768d9a6355cbd74f
zlcting <zlcting@gmail.com>
2018-01-06 17:40:07 +0800

    123123

--- baike/Application/Home/Controller/AdminController.class.php
+++ baike/Application/Home/Controller/AdminController.class.php
@@ -4,8 +4,30 @@ use Think\Controller;
 
 class AdminController extends Controller {
 
+
+    public function blank(){
+        echo "欢迎使用萌宠百科后台";
+    }
+
     public function index(){
         $this->assign('name',$name);
+        $this->display();
+    }
+
+    /**
+        顶级分类添加
+     **/
+    public function entryClass(){
+
+        $this->display();
+    }
+
+    public function entryClassLevel(){
+
+        $this->display();
+    }
+
+    public function buttons(){
         $this->display();
     }
 
