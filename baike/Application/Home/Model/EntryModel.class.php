77b1597e1b6309ba307b4c4398b85d7ff8ec3ae8
zlcting <zlcting@gmail.com>
2018-02-05 15:19:01 +0800

    citiao

---
+++ baike/Application/Home/Model/EntryModel.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Home\Model;
+use Think\Model;
+class EntryModel extends Model {
+	    protected $tablePrefix = 'baike_'; 
+	    protected $dbName = 'mcbaike';
+
+	    public function get(){
+	    	
+	    }
+
+}
